9384effdc4eaea8551a8dad9a5534b3aeda10be9
Papeya-taahaa <Taha7665>
2021-03-12 12:12:13 +0600

    arrays added

---
+++ arrays.js
@@ -0,0 +1,20 @@
+// fundamental javascript
+
+let friendsAge = [22 , 23 , 24 , 25 , 26 ];
+
+let tahaAge = friendsAge[3];
+console.log(tahaAge);
+
+// index element set
+let friendssAge = [22 , 23 , 24 , 25 , 26 ];
+friendssAge[3] = 21;
+console.log(friendssAge);
+
+// index position
+ let tourDays = [33 , 36 , 20 , 19 , 3];
+ let position = tourDays.indexOf(219);
+ console.log(position);
+
+
+
+
