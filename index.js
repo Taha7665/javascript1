ef3c0244b6d3e06fb7fde36e164683fc0ea648e7
Papeya-taahaa <Taha7665>
2021-03-12 10:44:19 +0600

    datatype

---
+++ index.js
@@ -0,0 +1,20 @@
+// data type
+var ApplePrice = "21";
+console.log (typeof ApplePrice);
+
+
+
+// variables
+
+let myPhoneNum = 01620447665;
+let houseNumber = 23;
+console.log (houseNumber);
+console.log (myPhoneNum);
+
+
+// 
+var runDistance = 26.2;
+console.log(runDistance);
+
+
+// booleans
